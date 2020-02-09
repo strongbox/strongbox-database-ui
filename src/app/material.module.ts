5348e60f93dee48070a0b9632ed665cde889b09f
Steve Todorov <steve.todorov@carlspring.com>
2020-02-09 21:54:28 +0200

    Post @angular/material upgrade fixes and optimizations

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,63 +1,28 @@
 import {NgModule} from '@angular/core';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {NgxJsonViewerModule} from 'ngx-json-viewer';
-
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
 
 import {CdkTableModule} from '@angular/cdk/table';
 
 const components = [
     CdkTableModule,
     FlexLayoutModule,
-    MatAutocompleteModule,
-    MatBadgeModule,
     MatButtonModule,
-    MatButtonToggleModule,
     MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatDialogModule,
     MatInputModule,
-    MatIconModule,
     MatListModule,
-    MatExpansionModule,
-    MatMenuModule,
-    MatPaginatorModule,
     MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
     MatSelectModule,
-    MatSidenavModule,
-    MatSlideToggleModule,
     MatTableModule,
     MatSortModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
     NgxJsonViewerModule
 ];
 
